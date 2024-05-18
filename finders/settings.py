b24dfb62242625c1762cc853dde1002580d6f4e7
sayad-mallllek <bob@khalilelzein.com>
2024-05-18 22:20:32 +0300

    feat: changed application to app for Vercel

--- finders/settings.py
+++ finders/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "finders.wsgi.application"
+WSGI_APPLICATION = "finders.wsgi.app"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
