03570f473bae870a18edd21533d4ea6b4f23e8ba
sayad-mallllek <bob@khalilelzein.com>
2024-05-18 22:28:01 +0300

    feat: first deploy

--- finders/settings.py
+++ finders/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-hp&jj($%ked64+8es$9+#ej$!^f0v7n#4y&0i*@9naof+ymox$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
